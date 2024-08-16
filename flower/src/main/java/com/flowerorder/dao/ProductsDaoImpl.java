2b283ca16fa05ab7bb06242d49b5897a7e0b4852
Ting Cheng <chen0869@algonquinlive.com>
2024-08-16 09:19:28 -0400

    Modify products-related classes to align with order-related design

--- flower/src/main/java/com/flowerorder/dao/ProductsDaoImpl.java
+++ flower/src/main/java/com/flowerorder/dao/ProductsDaoImpl.java
@@ -134,4 +134,23 @@ public class ProductsDaoImpl implements ProductsDao {
         }
     }
 
+    @Override
+    public String getProductNameById(int productId) {
+        String productName = null;
+        String query = "SELECT name FROM products WHERE product_id = ?";
+
+        try (Connection con = DBConnection.getConnection();
+             PreparedStatement pst = con.prepareStatement(query)) {
+            pst.setInt(1, productId);
+            
+            ResultSet rs = pst.executeQuery();
+            if (rs.next()) {
+                productName = rs.getString("name");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        return productName;
+    }
 }
